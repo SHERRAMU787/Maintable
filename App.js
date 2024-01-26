afe807f0f3f5dfe656dfdd98dc28974fdde7badc
ramu <ramusher143@gmail.com>
2024-01-26 09:45:01 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { Video, ResizeMode } from 'expo-av';
+import { Typography } from '@mui/material';
+import { Text} from 'react-native';
+
 
 export default function App() {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View 
+    style={{flex: 1,
+      justifyContent: 'center',
+      backgroundColor: '#ecf0f1',}}><View style={{alignItems:'center'}}>
+        <Text style={{fontWeight:'bold',alignItems:'center'}}>Demovideoplayer</Text></View>
+      <Video
+        ref={video}
+        style={{ alignSelf: 'center',
+        width: 320,
+        height: 200,}}
+        source={{
+          uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+        }}
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <View
+       style={{ flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop:15}}>
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
